343098b58a6c8063f4ab212a9854f67b22825b49
Chiru <hasenorc@gmail.com>
2025-05-17 17:55:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "23237831")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "017dc661ca1432ac2fe5ecb62499d88f")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7762712879:AAF3GaS5x9D-pG9iaLE249anyC8WACPl6to")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-token_cp = os.environ.get("token_cp", "")
-
+token_cp = os.environ.get("token_cp", "eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r")
